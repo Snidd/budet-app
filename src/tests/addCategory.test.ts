7ec7de6e5110152e003fb91567e6b4714dad4290
Magnus Kjellberg <magnus.kjellberg@istone.com>
2022-03-10 14:25:27 +0100

    Completed first test of adding categories.

--- src/tests/addCategory.test.ts
+++ src/tests/addCategory.test.ts
@@ -1,10 +1,14 @@
 import { addCategory } from '$lib/dal/addCategory';
+import { allCategories } from '$lib/stores/allCategories';
 import { describe, expect, it, vi } from 'vitest';
+import { get } from 'svelte/store';
 
 describe('adding categories', () => {
 	it('should add a category when called', async () => {
-		const spy = vi.mock('allCategories');
+		const spy = vi.mock('$lib/stores/allCategories');
 		await addCategory('test');
-		expect(spy).toHaveBeenCalledOnce();
+		const val = get(allCategories);
+		expect(val.length).toBe(1);
+		expect(val[0].name).toBe('test');
 	});
 });
