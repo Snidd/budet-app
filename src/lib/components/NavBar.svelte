94096c5a01b0af6e609960ddb59ad457d5346e06
Magnus Kjellberg <magnus.kjellberg@istone.com>
2022-03-16 16:48:19 +0100

    Added logo.

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -2,9 +2,12 @@
 	const aClasses = 'm-2 p-2 rounded-md hover:bg-slate-300';
 </script>
 
-<div class="flex gap-x-2 bg-slate-100">
+<div class="flex gap-x-2 bg-slate-100 items-center justify-end">
 	<a class={aClasses} href="/">Start</a>
 	<a class={aClasses} sveltekit:prefetch href="/budget">Budget</a>
 	<a class={aClasses} sveltekit:prefetch href="/categories">Kategorier</a>
 	<a class={aClasses} sveltekit:prefetch href="/invoices">Klarna</a>
+	<div class="h-12 ml-auto grow flex justify-end mr-2">
+		<img class="opacity-75" src="/mwm.svg" alt="mwm logo" />
+	</div>
 </div>
